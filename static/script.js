e4af9620937959a18c8257d2ce8fe0b2a77c0b06
Adrien Burgun <adrienburgun@hotmail.fr>
2022-03-29 08:54:05 +0900

    Ajout preview image de couverture

--- static/script.js
+++ static/script.js
@@ -7,3 +7,17 @@ setTimeout(() => {
   const box = document.getElementById('msg-modal-home');
   box.style.display = 'none';
 }, 2000);
+
+
+function previewBeforeUpload(){
+  document.getElementById("id_image").addEventListener("change",function(e){
+    if(e.target.files.length == 0){
+      return;
+    }
+    let file = e.target.files[0];
+    let url = URL.createObjectURL(file);
+    document.querySelector("#id_image img").src = url;
+  });
+}
+
+previewBeforeUpload();
